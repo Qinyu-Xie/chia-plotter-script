a296cb8d7e80ea2886d855d1e6ab6b99a719b4b4
sheldon <37561373+Qinyu-Xie@users.noreply.github.com>
2021-06-19 22:22:37 +0800

    Update chia-plotter-setup.sh

--- chia-plotter-setup.sh
+++ chia-plotter-setup.sh
@@ -4,8 +4,10 @@ sudo apt-get install cifs-utils
 sudo apt-get install nload
 
 ulimit -n 10000
-
 sudo mkdir /mnt/smb_d24_0/
+
+sudo mkdir /media/qinyu/ssd0/dest/
+# chmod -R a+rwX /mnt/smb_d24_0/
 
 sudo mount -t cifs //192.168.0.24/share /mnt/smb_d24_0/ -o uid=qinyu,gid=qinyu,guest
 
@@ -20,22 +22,28 @@ cd chia-plotter
 git submodule update --init
 ./make_devel.sh
 
-echo "nohup ./build/chia_plot -n -1 \-r 16 -u 128 -t /media/qinyu/ssd0/ -2 /media/qinyu/ssd1/ -d /mnt/smb_d24_0/ -p a795a9669ec652c07bd9a9e78c7c8317e76e8ee628af1759b8375b119e1b043460fac837916af2cb73372b59d68a6135 -f 824be3ba2cfb3d615f945a0ea7813c0bf7091ce385e001a6c784364e095cf43e3e575c14e63ccaabef025d48e74db377 &" > run_plotter.sh
+sudo mkdir -p ~/chia-plotter/sync/
 
 
 
+echo "nohup ./build/chia_plot -n -1 \-r 16 -u 128 -t /media/qinyu/ssd0/ -2 /media/qinyu/ssd1/ -d /media/qinyu/ssd0/dest/ -p a795a9669ec652c07bd9a9e78c7c8317e76e8ee628af1759b8375b119e1b043460fac837916af2cb73372b59d68a6135 -f 824be3ba2cfb3d615f945a0ea7813c0bf7091ce385e001a6c784364e095cf43e3e575c14e63ccaabef025d48e74db377 &" > ~/chia-plotter/sync/run_plotter.sh
 
 
 
+## run_plotter.sh dest need to bo motified
+sudo apt-get install -y inotify-tools
+mkdir /media/qinyu/ssd0/dest/
+mkdir ~/chia-plotter/sync/
+nohup mv ./*.plot /mnt/smb_d24_0/ &
 
 
+echo "
+inotifywait -m /media/qinyu/ssd0/dest/ -e create -e moved_to |
+    while read path action file; do
+        if [["$file" =~ .*plot$ ]]; then # Does the file end with .plot?
+            mv /media/qinyu/ssd0/dest/ /mnt/smb_d24_0/ # If so, do your thing here!
+        fi
+    done
+" > ~/chia-plotter/sync/sync-fg.sh
 
-
-
-
-
-
-
-
-# on plotter machine mkdir /mnt/smb_d24_0/
-# 	
+echo "nohup bash ./sync-fg.sh &" > ~/chia-plotter/sync/sync.sh
